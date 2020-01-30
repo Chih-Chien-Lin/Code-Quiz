9ebf8a02966d2ac8daef64939e640c06f041c217
Justin Lin <ro0566320@gmail.com>
2020-01-29 22:10:10 -0500

    finish highscore part!

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,304 +1,317 @@
 // variable for questions
 var questions = [
-    "1.Asdfjsldkjfvs;djf",
-    "2.Bdfasdfasdfsdafas",
-    "3.Cfasdfsadfasdfasd",
-    "4.Ddafasdgfgasdgasd",
-    "5.Esdfasdfasdfasdfd",
-    "6.Fasfasdfasdfasdfa"
-    /*
-      @TODO: write your questions here
-    */
-  ];
-  var ans1 = ["A.123","B.234","C.345","D.456"];
-  var ans2 = ["A.abc","B.bcd","C.cde","D.def"];
-  var ans3 = ["A.123","B.234","C.345","D.456"];
-  var ans4 = ["A.abc","B.bcd","C.cde","D.def"];
-  var ans5 = ["A.123","B.234","C.345","D.456"];
-  var ans6 = ["A.abc","B.bcd","C.cde","D.def"];
-  var ansTrue = [0,1,2,3,0,1];
-  var ansarray = [ans1,ans2,ans3,ans4,ans5,ans6];
-  // variables to keep track of quiz state
-  var time = questions.length * 15;
-  var currentQuestionIndex = 0;
-  var score = 0;
-  var timerId;
-  var question_w;
-  var button1;
-  var button2;
-  var button3;
-  var button4;
-  var button5;
-  var button6;
-  var popup;
-  var popup_w;
-  var nameScroe = [];
-  var i = 0;
-  /**
-   * Variables to reference DOM elements
-   * 
-   * @description
-   * You MAY want to consider the following elements:
-   *  - [x] A button to start the quiz
-   *  - [x] An element that displays the current time
-   *  - [x] A questions container that has:
-   *    - [ ] An element that displays the current question text
-   *    - [x] A container for the choices buttons
-   *  - [ ] An element that displays whether the user got a question correct or not
-   *  - [ ] An input field to allow the user to put in their initials 
-   *  - [x] A button to submit the user's high score
-   * 
-   * NOTE: Make sure your `index.html` elements correspond to these!
-   * 
-   * @see https://www.w3schools.com/jsref/met_document_getelementbyid.asp
-   */
-  var questionsEl = document.getElementById("questions");
-  var timerEl = document.getElementById("time");
-  var choicesEl = document.getElementById("choices");
-  var submitBtn = document.getElementById("submit");
-  var startBtn = document.getElementById("start");
-  var endScreen = document.getElementById("end-screen");
-  var finalScore = document.getElementById("final-score");
+  "1.Asdfjsldkjfvs;djf",
+  "2.Bdfasdfasdfsdafas",
+  "3.Cfasdfsadfasdfasd",
+  "4.Ddafasdgfgasdgasd",
+  "5.Esdfasdfasdfasdfd",
+  "6.Fasfasdfasdfasdfa"
+  /*
+    @TODO: write your questions here
+  */
+];
+var ans1 = ["A.123","B.234","C.345","D.456"];
+var ans2 = ["A.abc","B.bcd","C.cde","D.def"];
+var ans3 = ["A.123","B.234","C.345","D.456"];
+var ans4 = ["A.abc","B.bcd","C.cde","D.def"];
+var ans5 = ["A.123","B.234","C.345","D.456"];
+var ans6 = ["A.abc","B.bcd","C.cde","D.def"];
+var ansTrue = [0,1,2,3,0,1];
+var ansarray = [ans1,ans2,ans3,ans4,ans5,ans6];
+// variables to keep track of quiz state
+var time = questions.length * 15;
+var currentQuestionIndex = 0;
+var score = 0;
+var timerId;
+var question_w;
+var button1;
+var button2;
+var button3;
+var button4;
+var button5;
+var button6;
+var popup;
+var popup_w;
+var playerScore = [];
+var playerName = [];
+var i = 0;
+/**
+ * Variables to reference DOM elements
+ * 
+ * @description
+ * You MAY want to consider the following elements:
+ *  - [x] A button to start the quiz
+ *  - [x] An element that displays the current time
+ *  - [x] A questions container that has:
+ *    - [ ] An element that displays the current question text
+ *    - [x] A container for the choices buttons
+ *  - [ ] An element that displays whether the user got a question correct or not
+ *  - [ ] An input field to allow the user to put in their initials 
+ *  - [x] A button to submit the user's high score
+ * 
+ * NOTE: Make sure your `index.html` elements correspond to these!
+ * 
+ * @see https://www.w3schools.com/jsref/met_document_getelementbyid.asp
+ */
+var questionsEl = document.getElementById("questions");
+var timerEl = document.getElementById("time");
+var choicesEl = document.getElementById("choices");
+var submitBtn = document.getElementById("submit");
+var startBtn = document.getElementById("start");
+var endScreen = document.getElementById("end-screen");
+var finalScore = document.getElementById("final-score");
+
+/*
+  @TODO: write the rest of your variables here
+*/
+
+/**
+ * Function to start the quiz
+ * 
+ * @description
+ * This function does the following:
+ *  - [x] Hide/show page elements
+ *  - [x] Start the timer
+ *  - [x] Get the next question
+ */
+function startQuiz() {
+// console.log("quiz start!!")
+// console.log("currentQuestionIndex: "+currentQuestionIndex)
+  // hide start screen
+  var startScreenEl = document.getElementById("start-screen");
+  startScreenEl.setAttribute("class", "hide");
+
+  // un-hide questions container
+  questionsEl.removeAttribute("class");
+
+  // start timer
+  timerId = setInterval(clockTick, 1000);
+
+  // show starting time
+  // timerEl.textContent = time;
+
+  // call the function that gets the next question 
+  getQuestion();
+
+}
+
+
+/**
+ * Function to display next question
+ * 
+ * @description
+ * This function will:
+ *  - [ ] Retrieve next question and answers
+ *  - [ ] Update the page accordingly
+ * 
+ * @see https://www.w3schools.com/jsref/event_onclick.asp
+ * @see https://www.w3schools.com/js/js_htmldom_methods.asp
+ */
+function getQuestion() {
+  /*
+    @TODO: write your function code here
+  */
+  // create question and buttons
+  question_w = document.createElement("h2");
+  button1 = document.createElement("button");
+  button2 = document.createElement("button");
+  button3 = document.createElement("button");
+  button4 = document.createElement("button");
+  question_w.textContent = questions[currentQuestionIndex];
+  // set namme of choices to each button
+  button1.textContent = ansarray[currentQuestionIndex][0];
+  button2.textContent = ansarray[currentQuestionIndex][1];
+  button3.textContent = ansarray[currentQuestionIndex][2];
+  button4.textContent = ansarray[currentQuestionIndex][3];
+  // set data-index to each button
+  question_w.setAttribute("style","text-align:center")
+  button1.setAttribute("data-index", 0 );
+  button2.setAttribute("data-index", 1 );
+  button3.setAttribute("data-index", 2 );
+  button4.setAttribute("data-index", 3 );
+  // append question and button to html
+  questionsEl.prepend(question_w);
+  choicesEl.appendChild(button1);
+  choicesEl.appendChild(button2);
+  choicesEl.appendChild(button3);
+  choicesEl.appendChild(button4);
+}
+
+
+/**
+ * Function that runs when the user clicks on an answer
+ * 
+ * @description
+ * This function will:
+ *  - [ ] Check if the user picked the right answer or not, and behave accordingly
+ *  - [x] End quiz if no more questions left, or go onto next question
+ *
+ * HINT: for hiding/showing elements, take a look at the `.hide` class in
+ *  `styles.css`!
+ * 
+ * @see https://www.w3schools.com/jsref/met_win_settimeout.asp
+ * @see https://www.w3schools.com/jsref/met_element_setattribute.asp
+ * @see https://www.w3schools.com/jsref/met_element_removeattribute.asp
+ */
+function checkAns() {
+  /*
+    @TODO: write the rest of your function code here
+  */
+  // choicesEl.addEventListener("click",function(event){
+    event.preventDefault();
+    var selected = event.target;
+    var index = selected.getAttribute("data-index");
+    if (index == ansTrue[currentQuestionIndex]) {
+      popup = document.createElement("hr");
+      popup_w = document.createElement("h4");
+      popup_w.textContent = "You are correct!";
+      popup_w.setAttribute("style","color: blue")
+      choicesEl.appendChild(popup);
+      choicesEl.appendChild(popup_w);
+      score = score + 1;
+    }
+    else{
+      popup = document.createElement("hr")
+      popup_w = document.createElement("h4")
+      popup_w.textContent = "You are WRONG!"
+      popup_w.setAttribute("style","color: red")
+      choicesEl.appendChild(popup);
+      choicesEl.appendChild(popup_w);
+    } 
+  // let correct/ wrong message disappear in certain amount of time!
+  var timeleft = 3;
+      var timerInterval = setInterval(function() {
+          timeleft--;
+
+          if(timeleft === 0) {
+              choicesEl.removeChild(popup);
+              popup_w.textContent = "";
+              clearInterval(timerInterval);
+          }
+      }, 150);
+
+  // check if we've run out of questions
+  if (currentQuestionIndex === questions.length-1) {
+      quizEnd();
+    } else {
+      // add currentQustionIndex by 1, change to next question if is not the end of question list
+      currentQuestionIndex = currentQuestionIndex + 1;
+      //remove current question and choice buttons
+      questionsEl.removeChild(question_w);
+      choicesEl.removeChild(button1);
+      choicesEl.removeChild(button2);
+      choicesEl.removeChild(button3);
+      choicesEl.removeChild(button4);
+      getQuestion();
+    }
+
+}
+
+/**
+ * Function to end the quiz
+ * 
+ * @description
+ * This function will:
+ *  - [ ] Stop the timer
+ *  - [ ] Update the DOM accordingly
+ *
+ * HINT: for hiding/showing elements, take a look at the `.hide` class in
+ *  `styles.css`!
+ * 
+ * @see https://www.w3schools.com/jsref/met_win_clearinterval.asp
+ * @see https://www.w3schools.com/jsref/met_element_setattribute.asp
+ * @see https://www.w3schools.com/jsref/met_element_removeattribute.asp
+ */
+function quizEnd() {
+  /*
+    @TODO: write your function code here
+  */
+  clearInterval(timerId);
+  finalScore.textContent= score;
+  questionsEl.setAttribute("class", "hide");
+  choicesEl.setAttribute("class","hide");
+  endScreen.removeAttribute("class");
+  endScreen.setAttribute("style","text-align:center")
+}
+
+
+/**
+ * Function to handle the timer
+ * 
+ * @description
+ * This function will:
+ *  - [ ] Update the timer
+ *  - [x] End the quiz if the user runs out of time
+ */
+function clockTick() {
+  /*
+    @TODO: write the rest of your function code here
+  */
+      time--;
+      timerEl.textContent = time;
+      // end the quiz if the user runs out of time
+      if (time <= 0) {
+        quizEnd();
+      }
+}
+
+
+/**
+ * Function to save a new high score
+ * 
+ * @description
+ * This function will:
+ *  - [ ] Let user save their initials and high score
+ *  - [ ] Redirect to high scores page
+ * 
+ * @see https://www.w3schools.com/jsref/prop_text_value.asp
+ * @see https://www.w3schools.com/jsref/prop_win_localstorage.asp
+ * @see https://www.w3schools.com/howto/howto_js_redirect_webpage.asp
+ */
+function saveHighscore() {
+  var name = document.getElementById("user-name").value;
+  console.log("new player name: "+name);
+  var Name = JSON.parse(localStorage.getItem("Name"));
+  var Score = JSON.parse(localStorage.getItem("Score"))
+  console.log("stored player names: "+Name);
+  console.log("stored player scores: "+Score);
+  if(Name !== null){
+    playerName = Name;
+  }
+  if(Score !== null){
+    playerScore = Score;
+  }
+  
+  playerName.push(name);
+  console.log("latest player names: "+playerName);
+  playerScore.push(score);
+  console.log("latest player scores: "+playerScore);
+  localStorage.setItem("Name",JSON.stringify(playerName));
+  localStorage.setItem("Score",JSON.stringify(playerScore));
+
+  // nameScroe.push(name);
+  // nameScroe.push(score);
+  // storageInfo = JSON.parse(localStorage.getItem("nameScroe"))
+  //  console.log(nameScroe);
+  // if (storageInfo !== null){
+  //   nameScroe = storageInfo;
+  // }
+  
+  // nameScroe.push({name:name,score:score});
+  // localStorage.setItem("nameScore",JSON.stringify(nameScroe));
+  
+
+  location.replace("highscores.html");
   
   /*
-    @TODO: write the rest of your variables here
+    @TODO: write your function code here
   */
+  
+}
+// user clicks button to submit initials
+submitBtn.onclick = saveHighscore;
 
-  /**
-   * Function to start the quiz
-   * 
-   * @description
-   * This function does the following:
-   *  - [x] Hide/show page elements
-   *  - [x] Start the timer
-   *  - [x] Get the next question
-   */
-  function startQuiz() {
-  console.log("quiz start!!")
-  console.log("currentQuestionIndex: "+currentQuestionIndex)
-    // hide start screen
-    var startScreenEl = document.getElementById("start-screen");
-    startScreenEl.setAttribute("class", "hide");
-  
-    // un-hide questions container
-    questionsEl.removeAttribute("class");
-  
-    // start timer
-    timerId = setInterval(clockTick, 1000);
-  
-    // show starting time
-    // timerEl.textContent = time;
-  
-    // call the function that gets the next question 
-    getQuestion();
-  
-  }
-  
-  
-  /**
-   * Function to display next question
-   * 
-   * @description
-   * This function will:
-   *  - [ ] Retrieve next question and answers
-   *  - [ ] Update the page accordingly
-   * 
-   * @see https://www.w3schools.com/jsref/event_onclick.asp
-   * @see https://www.w3schools.com/js/js_htmldom_methods.asp
-   */
-  function getQuestion() {
-  console.log("question generation");
-    /*
-      @TODO: write your function code here
-    */
-    // create question and buttons
-    question_w = document.createElement("h2");
-    button1 = document.createElement("button");
-    button2 = document.createElement("button");
-    button3 = document.createElement("button");
-    button4 = document.createElement("button");
-    question_w.textContent = questions[currentQuestionIndex];
-    // set namme of choices to each button
-    button1.textContent = ansarray[currentQuestionIndex][0];
-    button2.textContent = ansarray[currentQuestionIndex][1];
-    button3.textContent = ansarray[currentQuestionIndex][2];
-    button4.textContent = ansarray[currentQuestionIndex][3];
-    // set data-index to each button
-    question_w.setAttribute("style","text-align:center")
-    button1.setAttribute("data-index", 0 );
-    button2.setAttribute("data-index", 1 );
-    button3.setAttribute("data-index", 2 );
-    button4.setAttribute("data-index", 3 );
-    // append question and button to html
-    questionsEl.prepend(question_w);
-    choicesEl.appendChild(button1);
-    choicesEl.appendChild(button2);
-    choicesEl.appendChild(button3);
-    choicesEl.appendChild(button4);
-  }
-  
-  
-  /**
-   * Function that runs when the user clicks on an answer
-   * 
-   * @description
-   * This function will:
-   *  - [ ] Check if the user picked the right answer or not, and behave accordingly
-   *  - [x] End quiz if no more questions left, or go onto next question
-   *
-   * HINT: for hiding/showing elements, take a look at the `.hide` class in
-   *  `styles.css`!
-   * 
-   * @see https://www.w3schools.com/jsref/met_win_settimeout.asp
-   * @see https://www.w3schools.com/jsref/met_element_setattribute.asp
-   * @see https://www.w3schools.com/jsref/met_element_removeattribute.asp
-   */
-  function checkAns() {
-    /*
-      @TODO: write the rest of your function code here
-    */
-    console.log("user click choice button");
-    // choicesEl.addEventListener("click",function(event){
-      event.preventDefault();
-      var selected = event.target;
-      var index = selected.getAttribute("data-index");
-      console.log("if true");
-      if (index == ansTrue[currentQuestionIndex]) {
-        popup = document.createElement("hr");
-        popup_w = document.createElement("h4");
-        popup_w.textContent = "You are correct!";
-        console.log("1")
-        popup_w.setAttribute("style","color: blue")
-        choicesEl.appendChild(popup);
-        choicesEl.appendChild(popup_w);
-        score = score + 1;
-      }
-      else{
-        popup = document.createElement("hr")
-        popup_w = document.createElement("h4")
-        popup_w.textContent = "You are WRONG!"
-        popup_w.setAttribute("style","color: red")
-        choicesEl.appendChild(popup);
-        choicesEl.appendChild(popup_w);
-      } 
-    // let correct/ wrong message disappear in certain amount of time!
-    var timeleft = 3;
-        var timerInterval = setInterval(function() {
-            timeleft--;
-
-            if(timeleft === 0) {
-                choicesEl.removeChild(popup);
-                popup_w.textContent = "";
-                clearInterval(timerInterval);
-            }
-        }, 300);
-  
-    // check if we've run out of questions
-    if (currentQuestionIndex === questions.length-1) {
-        quizEnd();
-      } else {
-        // add currentQustionIndex by 1, change to next question if is not the end of question list
-        currentQuestionIndex = currentQuestionIndex + 1;
-        //remove current question and choice buttons
-        questionsEl.removeChild(question_w);
-        choicesEl.removeChild(button1);
-        choicesEl.removeChild(button2);
-        choicesEl.removeChild(button3);
-        choicesEl.removeChild(button4);
-        getQuestion();
-      }
-  
-  }
-  
-  /**
-   * Function to end the quiz
-   * 
-   * @description
-   * This function will:
-   *  - [ ] Stop the timer
-   *  - [ ] Update the DOM accordingly
-   *
-   * HINT: for hiding/showing elements, take a look at the `.hide` class in
-   *  `styles.css`!
-   * 
-   * @see https://www.w3schools.com/jsref/met_win_clearinterval.asp
-   * @see https://www.w3schools.com/jsref/met_element_setattribute.asp
-   * @see https://www.w3schools.com/jsref/met_element_removeattribute.asp
-   */
-  function quizEnd() {
-    /*
-      @TODO: write your function code here
-    */
-    clearInterval(timerId);
-    finalScore.textContent= score;
-    questionsEl.setAttribute("class", "hide");
-    choicesEl.setAttribute("class","hide");
-    endScreen.removeAttribute("class");
-    endScreen.setAttribute("style","text-align:center")
-  }
-  
-  
-  /**
-   * Function to handle the timer
-   * 
-   * @description
-   * This function will:
-   *  - [ ] Update the timer
-   *  - [x] End the quiz if the user runs out of time
-   */
-  function clockTick() {
-    /*
-      @TODO: write the rest of your function code here
-    */
-        time--;
-        timerEl.textContent = time;
-        // end the quiz if the user runs out of time
-        if (time <= 0) {
-          quizEnd();
-        }
-  }
-  
-  
-  /**
-   * Function to save a new high score
-   * 
-   * @description
-   * This function will:
-   *  - [ ] Let user save their initials and high score
-   *  - [ ] Redirect to high scores page
-   * 
-   * @see https://www.w3schools.com/jsref/prop_text_value.asp
-   * @see https://www.w3schools.com/jsref/prop_win_localstorage.asp
-   * @see https://www.w3schools.com/howto/howto_js_redirect_webpage.asp
-   */
-  console.log("before save: "+nameScroe);
-  function saveHighscore() {
-    var name = document.getElementById("user-name").value;
-    nameScroe.push(name);
-    nameScroe.push(score);
-    
-    if(typeof(Storage) !== "undefined") {
-      if (localStorage.clickcount) {
-        localStorage.clickcount = Number(localStorage.clickcount)+1;
-      } 
-      else {
-        localStorage.clickcount = 1;
-      }
-    }
-    i = localStorage.clickcount;
-    localStorage.setItem("player"+i, JSON.stringify(nameScroe));
-
-    location.replace("highscores.html");
-    /*
-      @TODO: write your function code here
-    */
-    
-  }
-  console.log("after save: "+nameScroe);
-  // user clicks button to submit initials
-  submitBtn.onclick = saveHighscore;
-  
-  // user clicks button to start quiz
-  startBtn.onclick = startQuiz;
-  choicesEl.onclick = checkAns;
-  submitBtn.onclick = saveHighscore;
+// user clicks button to start quiz
+startBtn.onclick = startQuiz;
+choicesEl.onclick = checkAns;
+submitBtn.onclick = saveHighscore;
--- assets/js/scores.js
+++ assets/js/scores.js
@@ -11,13 +11,47 @@
  * @see https://www.w3schools.com/jsref/met_node_appendchild.asp
  * @see https://www.w3schools.com/jsref/met_document_createelement.asp
  */
+var playerScore = [];
+var playerName = [];
+var obj=[];
+var listPlayer = document.getElementById("highscores");
+var listdiv = document.createElement("div");
+listPlayer.appendChild(listdiv);
+
 function printHighscores() {
-  
+
     /*
       @TODO: write your function code here
     */
-   var name = JSON.parse(localStorage.getItem("history"));
-   console.log(name);
+   var name = JSON.parse(localStorage.getItem("Name"));
+   var score = JSON.parse(localStorage.getItem("Score"));
+   if(name !== null){
+    playerName = name;
+  }
+  if(score !== null){
+    playerScore = score;
+  }
+  for (var i = 0; i < playerName.length ; i ++){
+    obj[i] = {"name":playerName[i],
+              "score":playerScore[i]};
+  }
+  // console.log("unsoreted: "+obj);
+  obj.sort(function(a,b){
+    // return b.score - a.score;
+    if(a.score > b.score){
+      return -1;
+    }else{
+      return 1;
+    }
+  });
+console.log(obj);
+  for(var i = 0 ; i < obj.length; i++){
+    var pName = obj[i]["name"];
+    var pScore = obj[i]["score"];
+    var li = document.createElement("li");
+    li.textContent = pName+" "+pScore;
+    listdiv.appendChild(li);
+  }
 
   }
   
@@ -37,7 +71,8 @@ function printHighscores() {
     /*
       @TODO: write the rest of your function code here
     */
-  
+   localStorage.removeItem("Name")
+   localStorage.removeItem("Score")
     // reload window
     window.location.reload();
   
